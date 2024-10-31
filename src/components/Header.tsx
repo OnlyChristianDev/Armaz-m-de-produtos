7a4f6380ccb77fd7e8836b38eee79b179b011fed
christian <christiandeveloper123@gmail.com>
2024-10-31 18:29:10 -0300

    atualização do Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,48 @@
-import Image from "next/image"
-import Logo from "../images/Logo_para_Bazar_moderno-removebg-preview.png"
-import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import Image from "next/image";
+import Logo from "../images/Logo_para_Bazar_moderno-removebg-preview.png";
 
-export default function Header(){
-    const router = useRouter()
-    const venderRouter = () => {
-        router.push("/produtos")
-    }
+export default function Header() {
+    const [scrollY, setScrollY] = useState(0);
+    const router = useRouter();
+    const pathname = usePathname();
 
-    const sobreNosFunction = () => {
-        router.push("/sobreNos")
-    }
+    const handleScroll = () => {
+        setScrollY(window.scrollY);
+    };
 
-     
-    const ContatoFunction = () => {
-        router.push("/contatos")
-    }
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
-    return(
-        <header className="w-full flex items-center justify-center h-14 fixed top-0 left-0 right-0">
-            <Image src={Logo} width={60} height={60} alt="logo da loja" className="absolute left-2 cursor-pointer"/>
+    const links = [
+        { name: "Contato", path: "/contatos" },
+        { name: "Sobre Nós", path: "/sobreNos" },
+        { name: "Vender", path: "/produtos" }
+    ];
+
+    const isActive = (path) => pathname === path;
+
+    return (
+        <header className={`w-full flex items-center justify-center h-14 fixed top-0 left-0 right-0 z-50 ${scrollY > 1 ? "bg-white/30 border-white backdrop-blur-md" : "bg-transparent"}`}>
+            <Image src={Logo} width={60} height={60} alt="logo da loja" className="absolute left-2 cursor-pointer" />
             <ul className="flex gap-6">
-                <li onClick={ContatoFunction} className="cursor-pointer">Contato</li>
-                <li onClick={sobreNosFunction} className="cursor-pointer">Sobre Nós</li>
-                <li onClick={venderRouter} className="cursor-pointer">Vender</li>
+            {!isActive('/') && (
+                    <li onClick={() => router.push('/')} className="cursor-pointer">
+                        Início
+                    </li>
+                )}
+
+                {links.map((link) => (
+                    !isActive(link.path) && ( 
+                        <li key={link.path} onClick={() => router.push(link.path)} className="cursor-pointer">
+                            {link.name}
+                        </li>
+                    )
+                ))}
             </ul>
         </header>
-    )
+    );
 }
